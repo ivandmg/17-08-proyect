fef614e5cc4854cd9e0753461329d02ec4886184
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 10:17:26 -0400

    solucion completa MVC

---
+++ MVC.product/MVC.product/Models/AtaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVC.product.Models
+{
+    public class AtaContext:DbContext
+    {
+        public AtaContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVC.product.Models.product> products { get; set; }
+    }
+}
---
+++ MVC.product/MVC.product/Models/product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.product.Models
+{
+    public class product
+    {
+        [Key]
+        public int productID { get; set; }
+
+        [Required]
+        [Display(Description ="Product Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
